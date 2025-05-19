d287967d986add1ed70fd86c3cf7ce8629d1377d
nianod <arnoldkk422@gmail.com>
2025-05-19 21:49:27 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Tr from './Components/Tr'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <h1 className='font-bold bg-green-400'>Coding</h1>
+        
+        <Tr />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Tr.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Tr = () => {
+  return (
+    <div>
+      <h2 className='font blue bg-green-500 text-white '>hi</h2>
+    </div>
+  )
+}
+
+export default Tr
+
+
+
+
+
+
+
+
+
+
+
