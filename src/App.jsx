05a9367789325cc476a41d907417942f3abea569
nianod <arnoldkk422@gmail.com>
2025-05-23 22:18:02 +0300

    validated sign

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
-import SignUp from './Components/Signup'
-import Footer from './Components/Footer'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Layout from './Components/Layout';
+import Dashboard from './Components/Dashboard';
+import SignUp from './Components/Signup';
+import AuthLayout from './Components/authLayout';
 
 function App() {
-
   return (
-    <>
-      <div>
-        <SignUp />
-        <Footer />
-      </div>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
