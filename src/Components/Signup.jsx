4d09b4eec5cfceaaba3259833f92388c231c80c9
nianod <arnoldkk422@gmail.com>
2025-05-23 23:51:59 +0300

    validated sign

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -8,22 +8,25 @@ const SignUp = () => {
   const [username, setUsername] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("")
+  const [error, setError] = useState("")
 
 
   const handlESubmit = (event) => {
     event.preventDefault();
 
      if(password1 !== password2) {
-      alert("Password did not match")
-      return
+      setError("Passwords did not match")
+    } else if (password1.length < 6 || password2.length < 6) {
+      setError("Password too short")
+    } else {
+      setError("")
+      alert("registerd successfully")
     }
-
-    alert("registered succesfully")
     
   }
  
   return (
-    <div className='pb-20'>
+    <div className='container pb-20'>
       <form onSubmit={handlESubmit} className=' bg-blue-800 gap-1 mt-5 max-w-sm mx-auto p-4 flex flex-col text-white rounded-xl shadow-sm '>
         <h2 className='text-white font-bold text-center text-2xl'>Sign Up</h2>
          <label>Email: </label>
@@ -58,13 +61,14 @@ const SignUp = () => {
           required
           className='p-2 rounded bg-black focus:outline-transparent shadow-lg'
         />
+        {error && <p className='text-red-600 flex justify-center text-sm'>{error}</p> }
         <button otype="submit"
         className='bg-blue-300 p-2 cursor-pointer font-bold rounded text-xl mt-5'
         >
           Register
         </button>
-        <p>have an account?
-            <Link to='/Login'>
+        <p className='flex justify-center gap-1'>Have an account?
+            <Link to='/Login'className='text-red-500'>
               Login
             </Link>
            </p>
