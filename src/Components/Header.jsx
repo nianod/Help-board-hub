24378e9a7b607d640b83e9669b066d554f514885
nianod <arnoldkk422@gmail.com>
2025-05-23 14:52:50 +0300

    components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaLeaf, FaUser } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Header
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Tr = () => {
   return (
     <div>
-      <h2 className='font blue bg-green-500 text-white '>hi</h2>
+      
     </div>
   )
 }
