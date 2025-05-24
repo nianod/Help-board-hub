c1d3128cd5e1af8df086fa0d996488a17993b6ec
nianod <arnoldkk422@gmail.com>
2025-05-24 16:29:05 +0300

    completed register

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 import { FaLeaf, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 const navContentes = {
   imag: "/download.jpg",
   title: "Help Hub"
 }
 
 const Header = () => {
+
+  const [sideMenu, setSideMenu] = useState(false)
+  const toggles = () => {
+    setSideMenu(prev => !prev)
+  }
+
+  const handleLogout = () => {
+    alert("Logged out")
+    setSideMenu(false)
+  }
+
   return (
    <>
      <div className='bg-blue-700 flex  items-center justify-between p-2'>
@@ -17,7 +29,17 @@ const Header = () => {
           <h1 className='text-white text-2xl font-bold'>{navContentes.title}</h1>
         </div>
         <div>
-          {<FaUser />}
+          <button onClick={toggles}> {<FaUser />} </button>
+          {sideMenu && ( <div className='absolue right-0 w-30 bg-white rounded p-2 z-50'>
+            <Link to="/userProfile" className='block px-4 py-2' onClick={() => setSideMenu(false)}>
+            view Profile
+            </Link>
+            <button 
+              onClick={handleLogout}
+              className='w-full text-left px-4'
+            ></button>
+          </div>
+         )}
         </div>
       </div>
    </>   
