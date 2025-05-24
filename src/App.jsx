1c3f7b8a9b8f10126d26ba59b0308818fe3bfc80
nianod <arnoldkk422@gmail.com>
2025-05-24 19:29:54 +0300

    completed register

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './Components/Layout';
-import Dashboard from './Pages/Dashboard';
+import AuthLayout from './Components/authLayout';
+import Home from './Pages/Home';
 import SignUp from './Pages/Signup';
 import Signin from './Pages/Signin';
 import Reset from './Pages/Reset';
-import AuthLayout from './Components/authLayout';
-import Home from './Pages/Home';
+import Dashboard from './Pages/Dashboard';
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route element={<AuthLayout />}>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path='/login' element={<Signin />} />
-            <Route path="/reset" element={<Reset />} />
-          </Route>
-          <Route path='/' element={<Layout />}>
-          </Route>
+        <Route path="/" element={<Home />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/reset" element={<Reset />} />
+        </Route>
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
     </Router>
