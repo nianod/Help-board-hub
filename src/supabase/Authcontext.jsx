38f8e50128b8ba2a2e5602c8684364cb0988b068
nianod <arnoldkk422@gmail.com>
2025-05-26 21:39:18 +0300

    special commit

--- src/supabase/Authcontext.jsx
+++ src/supabase/Authcontext.jsx
@@ -6,6 +6,17 @@ const authContext = createContext()
 export const authContextProvider = ({ children }) => {
     const [session, setSession ] = useState(undefined)
 
+    useEffect(() => {
+        const{ data: listener} = supabase.auth.onAuthStateChange((_event, session) =>{
+            setSession(session)
+        })
+        return() => {
+        listener?.subscription.unsubscribe();
+    }
+    }, []);
+
+
+
     //Register new user
     const signupNewUser = async () => {
         const {data, error} = await supabase.auth.signUp({
@@ -20,4 +31,10 @@ export const authContextProvider = ({ children }) => {
         }
         return{success: true, data}
     }
+
+    return (
+        <authContext.Provider value={{session, signupNewUser}}>
+            {children}
+        </authContext.Provider>
+    )
 }
