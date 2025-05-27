4c91c66c7b43e53cd3b6e9a42fbfdd4bef6dbdf5
nianod <arnoldkk422@gmail.com>
2025-05-27 20:45:33 +0300

    special commit

--- src/Components/404.jsx
+++ src/Components/404.jsx
@@ -1,8 +1,9 @@
  
 const Notice = () => {
   return (
-    <div>
-      <h1 className='font-bold text-white'>404 not found </h1>
+    <div className="mt-9 flex flex-col justify-center items-center border-dotted border-2 p-5 w-fit m-auto">
+      <h1 className='font-bold text-black  flex items-center'>404 not found </h1>
+      <p className="font-bold text-black  flex items-center">The page has no content 🙁</p>
     </div>
   )
 }
