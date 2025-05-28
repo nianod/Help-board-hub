f618073ba488271f6f2753e44433742167a1b395
nianod <arnoldkk422@gmail.com>
2025-05-28 23:57:11 +0300

    real implementation

---
+++ src/Supabase/AuthContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useEffect, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [session, setSession] = useState(undefined)
+
+    return(
+       <AuthContext.Provider value={{ session }}> { children} </AuthContext.Provider>
+    )
+}
