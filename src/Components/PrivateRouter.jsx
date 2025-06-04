38de95d805239e70622ba9d511e59260b0525c6d
nianod <arnoldkk422@gmail.com>
2025-06-04 14:52:11 +0300

    private router

--- src/Components/PrivateRouter.jsx
+++ src/Components/PrivateRouter.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { userAuth } from '..supabase/AuthContext'
+import { Navigate } from 'react-router-dom'
 
-const PrivateRouter = () => {
+const PrivateRouter = ({ children }) => {
+  const { session } = userAuth()
   return (
-    <div>
-      
-    </div>
+    <>{session ? <>{children}</> :<Navigate to="/signup" />} </>
   )
 }
 
