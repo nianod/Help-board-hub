07b77b4be23317f8a0705697481950df7ab1c6fe
nianod <arnoldkk422@gmail.com>
2025-06-13 14:54:44 +0300

    helper done

--- src/Components/View.jsx
+++ src/Components/View.jsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom';
+import { FaBackward } from 'react-icons/fa';
 
 const View = () => {
   const location = useLocation();
@@ -16,6 +17,7 @@ const View = () => {
       <p>Author: {post.user_name}</p>
       <p className="text-sm text-gray-500">Contact via: {post.contact_method} - {post.contact_detail}</p>
       <p className="text-sm text-gray-400 mt-2">Posted on: {new Date(post.created_at).toLocaleString()}</p>
+      <span className='flex items-center gap-0.5'><FaBackward/> back </span>
     </div>
   );
 };
--- src/Pages/HelperDashboard.jsx
+++ src/Pages/HelperDashboard.jsx
@@ -1,1 +1,84 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '../libs/supabaseClient';  
+import { useNavigate } from 'react-router-dom'
+
+const HelperDashboard = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate()
+
+   useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('postst')
+          .select('*')
+          .order('created_at', { ascending: false });
+
+        if (error) throw error;
+
+        setPosts(data);
+      } catch (err) {
+        console.error('Error fetching posts:', err);
+        setError('Failed to load posts');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
  
+
+      //Accept post
+      const acceptPost = async() => {
+        alert("Accepted")
+      }  
+
+      //View POst 
+      const viewPost = (post) => {
+        navigate('/view', { state: { post }})
+      }
+
+  return (
+    <div className="relative min-h-screen p-4 pb-25 mt-20">
+      <h2 className='text-center font-bold text-xl text-green-500'>Available help Requests</h2>
+      <div className="mt-2 space-y-4">
+        {loading ? (
+          <p className='text-white'>Loading Help requests...</p>
+        ) : error ? (
+          <p className="text-red-500">{error}</p>
+        ) : posts.length === 0 ? (
+          <p className='text-white'>No requests yet.</p>
+        ) : (
+          posts.map(post => (
+            <div key={post.id} className="border p-4 rounded-lg shadow-sm bg-white">
+              <h3 className="font-bold text-lg capitalize">Category: {post.category}</h3>
+              <p className="mt-2 text-gray-700">{post.text}</p>  
+              <p className='mt-2'>Posted by: {post.user_name}</p>
+              {post.contact_method && (
+                <small className="block mt-1 text-gray-500">
+                  Contact via: {post.contact_method} - {post.contact_details}
+                </small>
+              )}
+              {post.created_at && (
+                <small className="block mt-1 text-gray-400">
+                  Posted on: {new Date(post.created_at).toLocaleString()}
+                </small>
+              )}
+              <div className='flex gap-1 justify-end  '>
+                <button onClick={() => viewPost(post)} className='p-1 rounded bg-blue-800 text-white cursor-pointer hover:bg-blue-700'>View</button>
+                <button onClick={() => acceptPost(post.id)} className='p-1 rounded bg-green-800 text-white cursor-pointer hover:bg-green-700'>Accept</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HelperDashboard;
