41f8e3996d06c7215b8bea01e084a99b780727f1
nianod <arnoldkk422@gmail.com>
2025-06-22 16:08:04 +0300

    removed markdown

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import ReactMarkdown from "react-markdown";
 
 export default function Home() {
-  const [markdown, setMarkdown] = useState("");
-
-  // Fetch the markdown file when component loads
-  useEffect(() => {
-    fetch("/docs/description.md")
-      .then((res) => res.text())
-      .then((text) => setMarkdown(text));
-  }, []);
-
   return (
     <div data-aos="zoom-out-up" className="text-center p-8">
       <h1 className="text-4xl font-bold mb-6 text-blue-500">Welcome to Help Hub!</h1>
       <p className="mb-8 text-white">Join us as:</p>
-
-      <div className="flex gap-4 justify-center mb-10">
+      <div className="flex gap-4 justify-center">
         <Link 
           to="/signup?role=helper" 
           className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600"
@@ -31,10 +19,8 @@ export default function Home() {
           I Need Help
         </Link>
       </div>
-
-      {/* Markdown Description */}
-      <div className="text-left max-w-xl mx-auto text-white">
-        <ReactMarkdown>{markdown}</ReactMarkdown>
+      <div>
+        {/*description using markdown */}
       </div>
     </div>
   );
