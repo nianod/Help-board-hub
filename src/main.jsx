8d3aafc37fb2a25a162415c6c07f7777f92323bc
nianod <arnoldkk422@gmail.com>
2025-07-09 20:07:46 +0300

    user prof

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthContextProvider } from './supabase/AuthContext.jsx'
+import { AuthContextProvider } from '../supabase/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
