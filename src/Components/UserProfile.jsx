07f1eb4f8b2c7b932a7a67026761bb121add483d
nianod <arnoldkk422@gmail.com>
2025-07-13 23:01:03 +0300

    role resovled

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -80,28 +80,28 @@ const UserProfile = () => {
         <h2 className='mt-6 text-white font-bold text-2xl'>Welcome to your Profile,
            <span className="capitalize text-3xl text-blue-200">{user?.user_metadata?.username || user?.email}</span></h2>
       </div>
-        <p>
+        <div>
           {role === 'seeker' && (
-            <span> How are you young man </span>
+            <div className="mt-4">
+               <h1>Your Previous Posts</h1>
+                {loading && <p>Loading posts...</p>}
+                {error && <p className='text-red-500'>Error: {error}</p>}
+                {profilePosts.length === 0 && !loading && <p>No posts found.</p>}
+                {profilePosts.map((post) => (
+                  <div key={post.id} className="bg-amber-500 text-green-600 p-4 rounded mb-2">
+                    <span>{post.category} Category</span>
+                    <p>Description: {post.text}</p>
+                    <span>Posted on: {new Date(post.created_at).toLocaleString()}</span>
+                  </div>
+                ))}
+              </div>            
           )}
 
-        </p>
+        </div>
         {role === 'helper' && (
-          <span>Thank you for helping</span>
+          <span>We are Glad you stood out to volunteer for providing Help Support</span>
         )}
-      {/* <div className="mt-4">
-        <h1>Your Previous Posts</h1>
-        {loading && <p>Loading posts...</p>}
-        {error && <p className='text-red-500'>Error: {error}</p>}
-        {profilePosts.length === 0 && !loading && <p>No posts found.</p>}
-        {profilePosts.map((post) => (
-          <div key={post.id} className="bg-amber-500 text-green-600 p-4 rounded mb-2">
-            <span>{post.category} Category</span>
-            <p>Description: {post.text}</p>
-            <span>Posted on: {new Date(post.created_at).toLocaleString()}</span>
-          </div>
-        ))}
-      </div> */}
+ 
       <div className="border-2 w-50 border-red-500 p-3 rounded flex items-center gap-2">
         <button className="text-red-400 flex items-center gap-2 cursor-pointer">Delete Account <FaTrash /></button>
       </div>
