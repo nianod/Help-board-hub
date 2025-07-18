4b69afd2c4b593db8f2b529db8aa361812e08e7d
nianod <arnoldkk422@gmail.com>
2025-07-18 18:05:22 +0300

    alert commit

--- src/Components/DeleteAccount.jsx
+++ src/Components/DeleteAccount.jsx
@@ -4,16 +4,16 @@ const DeleteAccount = ({ onCancel, onConfirm }) => {
     const [loading, setLoading] = useState(false)
     
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center  bg-opacity-70 shadow-md">
-            <div className="bg-gray-900 p-6 rounded-lg max-w-md w-full mx-4">
+        <div className="fixed inset-0 z-50 flex items-center justify-center  bg-opacity-70 ">
+            <div className="bg-gray-900 p-6 rounded-lg max-w-md w-full mx-4 shadow">
                 <h1 className="text-xl font-bold text-white mb-2">
-                    Are you sure you want to delete Account?
+                    Are you sure you want to delete Your Account?
                 </h1>
                 <span className="text-red-400 block mb-4">This Action is Irreversible!!</span>
                 
                 <div className="flex justify-end gap-3">
                     <button 
-                        className="px-4 py-2 bg-gray-600 hover:bg-gray-500 text-white rounded transition"
+                        className="px-4 py-2 bg-gray-600 hover:bg-gray-500 text-white rounded transition cursor-pointer"
                         onClick={onCancel}
                         disabled={loading}
                     >
